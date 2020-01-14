eb33c7d459909f777722e78ac09ea3ad1106988c
liujianhan <lorgerd@163.com>
2020-01-14 18:58:52 +0800

    update files.

--- snippets/variable_type_checker.py
+++ snippets/variable_type_checker.py
@@ -5,13 +5,13 @@
   @FileName     : variable_type_checker.py
   @Description  : Placeholder
 """
-import inspect
 from functools import wraps
+from inspect import signature
 
 
 def type_checker(func):
     annotations = func.__annotations__
-    sig = inspect.signature(func)
+    sig = signature(func)
 
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -24,11 +24,40 @@ def type_checker(func):
     return wrapper
 
 
+def type_assert(*ty_args, **ty_kwargs):
+    def decorate(func):
+        # If in optimized mode, disable type checking
+        if not __debug__:
+            return func
+
+        # Map function argument names to supplied types
+        sig = signature(func)
+        bound_types = sig.bind_partial(*ty_args, **ty_kwargs).arguments
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bound_values = sig.bind(*args, **kwargs)
+            # Enforce type assertions across supplied arguments
+            for name, value in bound_values.arguments.items():
+                if name in bound_types:
+                    if not isinstance(value, bound_types[name]):
+                        raise TypeError(
+                            'Argument {} must be {}'.format(name, bound_types[name])
+                        )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorate
+
+
 if __name__ == '__main__':
-    @type_checker
+    @type_assert(int, int)
     def add(a: int, b: int) -> int:
         while b:
             a, b = b, a % b
         return a
 
-    print(add(23.3, 2))
+
+    print(add(23, 2))
+    print(add(2,34))
