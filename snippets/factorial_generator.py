3a2b54145170c269849e1a3461b35fb8fc537589
liujianhan <lorgerd@163.com>
2020-01-17 18:03:37 +0800

    add files.

---
+++ snippets/factorial_generator.py
@@ -0,0 +1,20 @@
+"""
+  @Author       : liujianhan
+  @Date         : 2020/1/17 下午6:03
+  @Project      : DailyTinyImprovement
+  @FileName     : factorial_generator.py
+  @Description  : 生成器计算阶乘
+"""
+
+
+def fac(n=1):
+    fac_v = 1
+    while True:
+        fac_v *= n
+        yield fac_v
+        n += 1
+
+
+f = fac()
+
+next(f)
