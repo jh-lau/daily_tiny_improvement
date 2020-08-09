2f4b8ea8b55177a09997a4cd2b274c1502362d11
liujianhan <lorgerd@163.com>
2020-08-09 20:20:33 +0800

    add new demo file.

---
+++ Python进阶/异步并行与并发/futures_demo.py
@@ -0,0 +1,31 @@
+"""
+  @Author       : Liujianhan
+  @Date         : 20/8/9 14:16
+  @FileName     : futures_demo.py
+  @ProjectName  : DailyTinyImprovement
+  @Description  : Placeholder
+ """
+import time
+from concurrent import futures
+
+
+def sleep_func(args):
+    duration, flag = args
+    time.sleep(duration)
+    # print(f'Done after {duration} seconds with flag {flag}')
+    return duration, flag, f'{duration} >> {flag}'
+
+
+s = zip(range(10), range(11, 21))
+
+if __name__ == '__main__':
+    s1 = time.time()
+    # with futures.ProcessPoolExecutor() as pool:
+    #     for duration, flag, result in pool.map(sleep_func, s):
+    #         print(f"{duration} >> {flag} || {result}")
+
+    for t in s:
+        duration, flag, result = sleep_func(t)
+        print(f"{duration} >> {flag} || {result}")
+
+    print(f'Time cost: {time.time() - s1: .3f} seconds')
