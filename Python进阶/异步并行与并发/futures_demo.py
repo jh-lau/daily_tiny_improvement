2493c250ce16821c5235a57a037b64c81aed6104
liujianhan <lorgerd@163.com>
2020-08-10 23:24:23 +0800

    add new demo file.

--- Python进阶/异步并行与并发/futures_demo.py
+++ Python进阶/异步并行与并发/futures_demo.py
@@ -20,12 +20,13 @@ s = zip(range(10), range(11, 21))
 
 if __name__ == '__main__':
     s1 = time.time()
-    # with futures.ProcessPoolExecutor() as pool:
-    #     for duration, flag, result in pool.map(sleep_func, s):
-    #         print(f"{duration} >> {flag} || {result}")
+    # 设定并行使用的cpu数量
+    with futures.ProcessPoolExecutor(max_workers=5) as pool:
+        for duration, flag, result in pool.map(sleep_func, s):
+            print(f"{duration} >> {flag} || {result}")
 
-    for t in s:
-        duration, flag, result = sleep_func(t)
-        print(f"{duration} >> {flag} || {result}")
+    # for t in s:
+    #     duration, flag, result = sleep_func(t)
+    #     print(f"{duration} >> {flag} || {result}")
 
     print(f'Time cost: {time.time() - s1: .3f} seconds')
