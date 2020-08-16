57e0cda223604abda8bce3a9160484b5c9576406
liujianhan <lorgerd@163.com>
2020-08-16 13:10:26 +0800

    add new demo file.

---
+++ snippets/rename_file.py
@@ -0,0 +1,18 @@
+"""
+  @Author       : Liujianhan
+  @Date         : 20/8/16 13:08
+  @FileName     : rename_file.py
+  @ProjectName  : DailyTinyImprovement
+  @Description  : 将指定文件夹下的类似01-链表重命名为链表
+ """
+import os
+
+
+if __name__ == '__main__':
+    dst_path = os.path.abspath('02-算法思想')
+
+    for d in os.listdir(dst_path):
+        src = os.path.realpath(os.path.join(dst_path, d))
+        temp = d.split('-')[1]
+        dst = os.path.realpath(os.path.join(dst_path, temp))
+        os.rename(src, dst)
