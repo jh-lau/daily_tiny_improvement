33910a9162e2e17ddcd5db73ed0c5115c6f98d69
liujianhan <liujianhan@gmail.com>
2020-10-29 11:34:06 +0800

    add new file.

--- Python进阶/常用库函数/heapq_demo.py
+++ Python进阶/常用库函数/heapq_demo.py
@@ -11,10 +11,11 @@ if __name__ == '__main__':
     s = []
     heapq.heappush(s, 5)
     heapq.heappush(s, 3)
-    heapq.heappush(s, 7)
+    heapq.heappush(s, 3)
     heapq.heappush(s, 4)
     # 元素总是会被按照从高（数值小的树优先级高）到低的顺序从堆中被弹出
     print(heapq.heappop(s))
-    print(heapq.heappop(s))
-    print(heapq.heappop(s))
-    print(heapq.heappop(s))
+    print(heapq.nsmallest(3, s))
+    # print(heapq.heappop(s))
+    # print(heapq.heappop(s))
+    # print(heapq.heappop(s))
---
+++ Python进阶/常用库函数/trick_or_trap/是否可哈希.py
@@ -0,0 +1,24 @@
+"""
+  @Author       : liujianhan
+  @Date         : 2020/10/29 11:14
+  @Project      : DailyTinyImprovement
+  @FileName     : 是否可哈希.py
+  @Description  : Placeholder
+"""
+from typing import Any
+
+
+def hashable(obj: Any) -> bool:
+    try:
+        hash(obj)
+        return True
+    except TypeError as e:
+        return False
+
+
+if __name__ == '__main__':
+    print(hashable([1, 23, 3]))
+    print(hashable((1, 2, 3, 4)))
+    print(hashable((1, 2, 3, 4, [12, 3])))
+    print(hashable((1, 2, 3, 4, {123, 2, 3})))
+    print([1, 2, 3] == [1, 2, 3])
