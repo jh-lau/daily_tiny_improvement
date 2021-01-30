548a25eadf3614803664d8bcd6838954d739f456
liujianhan <lorgerd@163.com>
2021-01-30 09:58:11 +0800

    reorganize files.

--- 实用库/命令行参数与配置/absl_flags/absl_demo.py
+++ 实用库/命令行参数与配置/absl_flags/absl_demo.py
@@ -6,20 +6,30 @@
   @Description  : Placeholder
 """
 from absl import app
-from absl import flags
+from absl import flags, logging
 
-flag = flags.FLAGS
 flags.DEFINE_string('name', None, 'Your name.')
 flags.DEFINE_integer('num_times', 3, 'Number of times to print greeting.')
 
 flags.mark_flag_as_required('name')
 
+FLAGS = flags.FLAGS
+
+
+class Test:
+    def __init__(self):
+        if FLAGS.name:
+            for _ in range(FLAGS.num_times):
+                logging.info(f"Hello, {FLAGS.name}")
+
 
 def main(argv):
     del argv
-    for i in range(flag.num_times):
-        print(f"Hello, {flag.name}")
+    for i in range(FLAGS.num_times):
+        print(f"Hello, {FLAGS.name}")
 
 
 if __name__ == '__main__':
-    app.run(main)
+    # app.run(main)
+    print(FLAGS.name)
+    print(FLAGS.num_times)
