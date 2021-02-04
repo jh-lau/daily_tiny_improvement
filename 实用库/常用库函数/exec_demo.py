628c8f7c93217c3cb66b69ce8a1d8f77d837e7b0
liujianhan <liujianhan@gmail.com>
2021-02-04 15:54:04 +0800

    update files.

---
+++ 实用库/常用库函数/exec_demo.py
@@ -0,0 +1,16 @@
+"""
+  @Author       : liujianhan
+  @Date         : 21/2/4 15:52
+  @Project      : DailyTinyImprovement
+  @FileName     : exec_demo.py
+  @Description  : Placeholder
+"""
+
+
+if __name__ == '__main__':
+    s = 'print("hello world")'
+    t = """for i in range(5):
+        print(f"iter time: {i}")
+    """
+    exec(s)
+    exec(t)
