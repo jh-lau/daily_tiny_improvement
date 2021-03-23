1e518abf62e4b7af0ea34205e0a2e1f36f294de1
liujianhan <liujianhan@gmail.com>
2021-03-23 09:40:11 +0800

    feat: Add new snippet.

---
+++ 代码片段/工具类/展开嵌套的序列.py
@@ -0,0 +1,32 @@
+"""
+  @Author       : liujianhan
+  @Date         : 21/3/23 9:36
+  @Project      : DailyTinyImprovement
+  @FileName     : 展开嵌套的序列.py
+  @Description  : Placeholder
+"""
+from collections import Iterable
+
+
+def flatten(items, ignore_types=(bytes, str)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+def flatten_for(items, ignore_types=(bytes, str)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            for i in flatten(x):
+                yield i
+        else:
+            yield x
+
+
+if __name__ == '__main__':
+    items = [1, 2, [3, 4, [5, 6], 7], 8]
+    # items = ['Dave', 'Paula', ['Thomas', 'Lewis']]
+    print([x for x in flatten([items])])
+    print([x for x in flatten_for([items])])
