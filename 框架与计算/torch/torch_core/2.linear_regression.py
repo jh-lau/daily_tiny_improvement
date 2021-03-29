50cd79eb06dd413bb2a622ebd32f74a45e90d03d
liujianhan <liujianhan@gmail.com>
2021-03-29 15:14:15 +0800

    feat: Add new snippet.

--- 框架与计算/torch/torch_core/2.linear_regression.py
+++ 框架与计算/torch/torch_core/2.linear_regression.py
@@ -10,23 +10,28 @@ import torch as th
 
 if __name__ == '__main__':
     x = th.rand(20, 1) * 10
-    y = 2 * x + (5 + th.randn(20, 1))
+    w, b = 2, 5
+    y = w * x + (b + th.randn(20, 1))
 
     lr = 1e-3
     w = th.randn((1,), requires_grad=True)
     b = th.zeros((1,), requires_grad=True)
 
-    for i in range(100):
+    for i in range(100000):
         wx = th.mul(w, x)
         y_pred = th.add(wx, b)
 
+        # 计算损失
         loss = (.5 * (y - y_pred) ** 2).mean()
 
+        # 反向传播
         loss.backward()
 
-        b.data.sub_(lr * b.grad)
+        # 梯度更新
         w.data.sub_(lr * w.grad)
+        b.data.sub_(lr * b.grad)
 
+        # 梯度清零
         w.grad.data.zero_()
         b.grad.data.zero_()
 
