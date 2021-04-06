6bd99ef6c63715735faa83717bbea9380f9fe7c2
liujianhan <liujianhan@gmail.com>
2021-04-06 10:36:33 +0800

    feat: modify snippet.

--- 实用库/1_标准库/24_Python运行时服务/context_manager_上下文管理器/custom_catcher.py
+++ 实用库/1_标准库/24_Python运行时服务/context_manager_上下文管理器/custom_catcher.py
@@ -7,9 +7,44 @@
 """
 import contextlib
 import functools
-from typing import Callable
+import logging
+import os
 
-from logger_demo_日志设置 import get_logger
+
+def get_logger(log_name: str,
+               file_path: str = '',
+               file_level=logging.INFO,
+               console_level=logging.INFO,
+               filemode="w",
+               to_screen: bool = True):  # a,追加, w,覆蓋
+    file_path = file_path or os.path.join(os.path.dirname(__file__))
+    dest_dir = file_path + "/log"
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)  # 定义要创建的目录
+
+    # log_name = 'log/' + log_name
+    logger = logging.getLogger(log_name)
+    # 此处设定等级后，后续的handler再设等级只有比此等级高才有效，否则无效，如此处为INFO，后续的DEBUG将无效
+    logger.setLevel(file_level)
+
+    formatter = logging.Formatter('%(asctime)s '
+                                  '%(levelname)s '
+                                  '[%(filename)s:%(lineno)d:%(funcName)s:%(threadName)s] '
+                                  ': %(message)s')
+
+    file_handler = logging.FileHandler(os.path.join(dest_dir, log_name + '.log'), encoding="utf-8",
+                                       mode=filemode)
+    file_handler.setLevel(file_level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    if to_screen:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(console_level)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    return logger
 
 
 class NeedResetError(Exception):
