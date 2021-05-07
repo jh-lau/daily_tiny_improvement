21b972b14d0f1587e4e0d81dbd9b22ba33b53427
liujianhan <liujianhan@gmail.com>
2021-05-07 14:53:04 +0800

    feat: modify snippet.

--- 实用库/4_自动化/absl_sacred_命令行参数与配置/absl_flags/absl_demo.py
+++ 实用库/4_自动化/absl_sacred_命令行参数与配置/absl_flags/absl_demo.py
@@ -8,28 +8,26 @@
 from absl import app
 from absl import flags, logging
 
+FLAGS = flags.FLAGS
+
 flags.DEFINE_string('name', None, 'Your name.')
 flags.DEFINE_integer('num_times', 3, 'Number of times to print greeting.')
 
 flags.mark_flag_as_required('name')
-
-FLAGS = flags.FLAGS
 
 
 class Test:
     def __init__(self):
         if FLAGS.name:
             for _ in range(FLAGS.num_times):
-                logging.info(f"Hello, {FLAGS.name}")
+                logging.info(f"Test, {FLAGS.name}")
 
 
 def main(argv):
     del argv
     for i in range(FLAGS.num_times):
-        print(f"Hello, {FLAGS.name}")
+        print(f"{i} -- Hello, {FLAGS.name}")
 
 
 if __name__ == '__main__':
-    # app.run(main)
-    print(FLAGS.name)
-    print(FLAGS.num_times)
+    app.run(main)
