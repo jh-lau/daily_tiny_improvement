c73d508567fd7712e0bdeb33bee395a5e93240e3
liujianhan <lorgerd@163.com>
2021-07-11 23:09:43 +0800

    docs: update files.

---
+++ 实用库/2_数据分析与可视化/pandas_数据处理/tuple_2_dataframe.py
@@ -0,0 +1,15 @@
+"""
+  @Author       : liujianhan
+  @Date         : 21/7/11 23:07
+  @Project      : DailyTinyImprovement
+  @FileName     : tuple_2_dataframe.py
+  @Description  : Placeholder
+"""
+
+import pandas as pd
+
+if __name__ == '__main__':
+    file_path = '../../test.csv'
+    df = pd.read_csv(file_path)
+    new_df = pd.DataFrame(list(df.age.apply(lambda x: (x + 1, x - 1))), columns=['age', 'old_age'])
+    df = pd.concat([df, new_df], axis=1)
