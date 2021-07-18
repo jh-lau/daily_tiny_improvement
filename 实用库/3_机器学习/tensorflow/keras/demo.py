f2ab6f40c39765c09199c42d6779180027d7e824
liujianhan <lorgerd@163.com>
2021-07-18 21:41:43 +0800

    docs: update files.

--- 实用库/3_机器学习/tensorflow/keras/demo.py
+++ 实用库/3_机器学习/tensorflow/keras/demo.py
@@ -9,4 +9,5 @@ import tensorflow as tf
 
 if __name__ == '__main__':
     filepath_dataset = tf.data.Dataset.list_files('data/covtype_data')
+    tf.feature_column.embedding_column()
     print(filepath_dataset)
