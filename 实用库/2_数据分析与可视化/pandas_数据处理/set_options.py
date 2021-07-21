79e2899c7a24cdb784cff52f67a3ffa8eeb84b15
jh-lau <liujianhan@gmail.com>
2021-07-21 10:30:53 +0800

    feats: add new files.

---
+++ 实用库/2_数据分析与可视化/pandas_数据处理/set_options.py
@@ -0,0 +1,14 @@
+"""
+  @Author       : liujianhan
+  @Date         : 2021/7/21 10:30
+  @Project      : DailyTinyImprovement
+  @FileName     : set_options.py
+  @Description  : Placeholder
+"""
+import pandas as pd
+
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.max_columns', None)
+
+if __name__ == '__main__':
+    pass
