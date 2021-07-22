5917b2370cb15d56caef5ea594a9bd438b3ce07b
jh-lau <liujianhan@gmail.com>
2021-07-22 15:50:18 +0800

    feats: add new files.

---
+++ 实用库/1_标准库/set_options.py
@@ -0,0 +1,13 @@
+"""
+  @Author       : liujianhan
+  @Date         : 2021/7/22 15:49
+  @Project      : DailyTinyImprovement
+  @FileName     : set_options.py
+  @Description  : Placeholder
+"""
+import numpy as np
+
+np.set_printoptions(precision=3)
+
+if __name__ == '__main__':
+    pass
