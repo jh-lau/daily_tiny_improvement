bd3b3f573611610edccbf6bf4572926104a6deed
liujianhan <lorgerd@163.com>
2021-09-05 00:54:11 +0800

    docs: update files.

--- 实用库/5_网络爬虫/requests/demo.py
+++ 实用库/5_网络爬虫/requests/demo.py
@@ -5,3 +5,15 @@
   @FileName     : demo.py
   @Description  :
 """
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    url = 'https://book.douban.com/subject/24531334/'
+    header = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+    }
+    r = requests.get(url, headers=header, verify=False)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    target = [t['src'] for t in soup.find_all('img') if t.attrs.get('title') == '点击看大图']
+    pass
