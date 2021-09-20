a9b743c4e59006f37129b603c22488ba640f2c03
liujianhan <lorgerd@163.com>
2021-09-20 09:31:19 +0800

    docs: update files.

--- 实用库/1_标准库/0_内置类型/iter_sentinel.py
+++ 实用库/1_标准库/0_内置类型/iter_sentinel.py
@@ -6,7 +6,7 @@
   @Description  : iter的额外用法：
     iter(iterable) -> iterator
     iter(callable, sentinel) -> iterator
-    出现哨符后停止，哨符不会被返回，相当于stopiteration的触发条件
+    出现哨符后停止，哨符不会被返回，相当于stopIteration的触发条件
 """
 from random import randint
 
