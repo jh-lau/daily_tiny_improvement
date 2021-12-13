b782e7d68f76c43bb24b3627bb69f05dcaa87a1e
jh-lau <liujianhan@gmail.com>
2021-12-13 16:00:57 +0800

    feats: add new files.

---
+++ 实用库/1_标准库/11_通用操作系统服务/simple_logger.py
@@ -0,0 +1,13 @@
+"""
+  @Author       : liujianhan
+  @Date         : 21.12.9 15:11
+  @Project      : DailyTinyImprovement
+  @FileName     : simple_logger.py
+  @Description  : Placeholder
+"""
+import logging
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    logger.info('hello world')
